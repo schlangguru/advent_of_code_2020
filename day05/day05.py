f9c3b5ec449acfa0fbaf5664c1770f510bbb685d
Sebastian <sebastianseidl@yahoo.de>
2020-12-05 08:44:27 +0100

    Day05 - Part02

--- day05/day05.py
+++ day05/day05.py
@@ -32,6 +32,33 @@ def part01(boarding_passes):
 
     return max(seat_ids)
 
+
+def part02(boarding_passes):
+    rows = list(range(ROW_COUNT))
+    cols = list(range(COL_COUNT))
+
+    possible_seats = []
+    for row in rows:
+        for col in cols:
+            possible_seats.append((row, col))
+
+    taken_seats = []
+    for boarding_pass in boarding_passes:
+        row_indicators = boarding_pass[0:7]
+        col_indicators = boarding_pass[7:10]
+
+        [row] = reduce(binary_search, row_indicators, rows)
+        [col] = reduce(binary_search, col_indicators, cols)
+        taken_seats.append((row, col))
+
+    free_seats = [seat for seat in possible_seats if seat not in taken_seats]
+    free_seat_ids = list(map(lambda row_col: seat_id(row_col[0], row_col[1]), free_seats))
+    taken_seat_ids = list(map(lambda row_col: seat_id(row_col[0], row_col[1]), taken_seats))
+    my_seat = list(filter(lambda seat_id: (seat_id+1) in taken_seat_ids and (seat_id-1) in taken_seat_ids, free_seat_ids))
+
+    return my_seat[0]
+
+
 def main():
     working_dir = os.path.dirname(__file__)
     input_file = os.path.join(working_dir, "input.txt")
@@ -41,6 +68,7 @@ def main():
         lines = [line.strip() for line in f.readlines()]
 
     print("Part01 - Highest seat ID:", part01(lines))
+    print("Part02 - My Seat ID:", part02(lines))
 
 if __name__ == "__main__":
     main()
