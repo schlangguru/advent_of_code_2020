fc09e1840dd19b44292962f05fea71107290b513
Sebastian <sebastianseidl@yahoo.de>
2020-12-06 19:29:21 +0100

    Day06 - Part02

--- day06/day06.py
+++ day06/day06.py
@@ -8,16 +8,31 @@ def parse_answers(input):
     return list(map(parse_group_answers, group_answers))
 
 def parse_group_answers(input):
-    return [answer.strip() for answer in input.split("\n") if len(answer) > 0]
+    group_answers = [answer.strip() for answer in input.split("\n") if len(answer) > 0]
+    return list(filter(lambda x: x != "", group_answers))
 
 def count_any_group_yes(group_answers):
     combined_answers = reduce(add, group_answers, "")
     return len(set(combined_answers))
 
+def count_group_yes(group_answers):
+    if len(group_answers) == 1:
+        return len(group_answers[0])
+    intersected_answers = reduce(intersect, group_answers)
+    if len(intersected_answers) > 0:
+        return len(intersected_answers)
+    return 0
+
+def intersect(list1, list2):
+    return [x for x in list1 if x in list2]
+
 def part01(input):
     answers = parse_answers(input)
     return sum(map(count_any_group_yes, answers))
 
+def part02(input):
+    answers = parse_answers(input)
+    return sum(map(count_group_yes, answers))
 
 def main():
     working_dir = os.path.dirname(__file__)
@@ -28,6 +43,7 @@ def main():
         content = f.read()
 
     print("Part01 - Result:", part01(content))
+    print("Part02 - Result:", part02(content))
 
 if __name__ == '__main__':
     main()
--- day06/test.py
+++ day06/test.py
@@ -25,5 +25,27 @@ class TestDay06(unittest.TestCase):
         self.assertEqual(day06.part01(input), 11)
 
 
+    def test_part02(self):
+        input = """
+            abc
+
+            a
+            b
+            c
+
+            ab
+            ac
+
+            a
+            a
+            a
+            a
+
+            b
+        """
+
+        self.assertEqual(day06.part02(input), 6)
+
+
 if __name__ == '__main__':
     unittest.main()
